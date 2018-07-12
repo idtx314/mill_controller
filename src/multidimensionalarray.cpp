df35eb41d72ae4385292a3bfb81d6a31cc5157d8
Ian Davis <idtx314business@gmail.com>
2018-07-12 17:31:35 -0500

    User input and looping array initialization allow for arrays of variable size. Size may be fixed in the final program, since measurements will be taken from a limited area, but using this method will allow flexibility.

--- src/multidimensionalarray.cpp
+++ src/multidimensionalarray.cpp
@@ -10,18 +10,37 @@ int main(int argc, char** argv)
     if(argc != 4)
         return 1;
 
-    int COL,ROW,DEP;
+    // Define and initialize variables
+    int COL,ROW,DEP,counter=0;
 
+    // Save arguments into variables
     sscanf(argv[1], "%d", &COL);
     sscanf(argv[2], "%d", &ROW);
     sscanf(argv[3], "%d", &DEP);
 
     // std::cout << (char)COL << " " << (char)ROW << " " << (char)DEP << std::endl;
 
+    // Echo input
     printf("%d %d %d\n",COL, ROW, DEP);
 
-    int arr[DEP][ROW][COL] = {{{0,1},{2,3}},{{4,5},{6,7}}};
+    // Define array (can also initialize here if contents are known)
+    // int arr[DEP][ROW][COL] = {{{0,1},{2,3}},{{4,5},{6,7}}};
+    int arr[DEP][ROW][COL];
 
+    // Initialize array
+    for(int z=0; z<DEP; z++) //Cycle through depths
+    {
+        for(int y=0; y<ROW; y++) //Cycle through rows
+        {
+            for(int x=0; x<COL; x++) //Cycle through columns
+            {
+                arr[z][y][x] = counter;
+                counter++;
+            }
+        }
+    }
+
+    // Print array
     for(int z=0; z<DEP; z++) //Cycle through depths
     {
         for(int y=0; y<ROW; y++) //Cycle through rows
