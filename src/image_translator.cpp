5fb50dddf4482c539167f7dfd56db1d54ee9d1ce
Ian Davis <idtx314business@gmail.com>
2018-11-07 00:52:09 -0600

    image translator is now correctly taking in an image and outputting a point cloud based on the positions of white pixels in the image. Working on hooking this to the rest of the pipeline.

--- src/image_translator.cpp
+++ src/image_translator.cpp
@@ -6,6 +6,7 @@
 #include<pcl_conversions/pcl_conversions.h>
 #include<pcl/point_cloud.h>
 #include<pcl/point_types.h>
+#include<pcl/visualization/pcl_visualizer.h>
 
 //TODO
 /*
@@ -18,14 +19,18 @@ ros::Publisher pub;
 // Declare image pointer
 cv_bridge::CvImagePtr cv_ptr;
 
+// Create a visualizer
+boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
+
 
 void callback(const sensor_msgs::Image &msg)
 {
     // Accepts an image message, generates a pointcloud from it, translates that pointcloud into a pointcloud message, and publishes it.
 
+    // Translate the image message to a cv_bridge image container
     try
     {
-        // Returns a cv::Mat
+        // Returns a container with a cv::Mat
         cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
     }
     catch (cv_bridge::Exception& e)
@@ -34,19 +39,71 @@ void callback(const sensor_msgs::Image &msg)
         return;
     }
 
-    ROS_INFO("Received");
-    cv::Vec3b intensity = cv_ptr->image.at<cv::Vec3b>(400, 600); // Row, Col
 
-    ROS_INFO("Val: %d  Col: %d  Row: %d", intensity.val[0], cv_ptr->image.cols, cv_ptr->image.rows);
-
-
+    // Declare a point cloud pointer and a point
     pcl::PointCloud<pcl::PointXYZ>::Ptr basic_cloud_ptr (new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PointXYZ basic_point;
+    // Set z to constant 0
+    basic_point.z = 0;
 
 
+    // Cycle through cv_ptr->image.cols, cv_ptr->image.rows pixels and read intensity
+    int colnum = cv_ptr->image.cols;
+    int rownum = cv_ptr->image.rows;
+    cv::Vec3b intensity;
+
+    for(int i = 0; i < rownum; i++)
+    {
+        for(int j = 0; j < colnum; j++)
+        {
+            // Row, Col
+            intensity = cv_ptr->image.at<cv::Vec3b>(i, j);
+
+            // Access in intensity.val[i], with i being 0,1,2 for b,g,r. uchar format. If a pixel is not black
+            if(intensity.val[0] > 0)
+            {
+                // Add a point to the cloud representing the current pixel
+                basic_point.x = j;
+                basic_point.y = i;
+                basic_cloud_ptr->points.push_back(basic_point);
+
+            }
+        }
+    }
+
+    // Set some properties of the cloud to make a valid message?
+    basic_cloud_ptr->width = (int) basic_cloud_ptr->points.size ();
+    basic_cloud_ptr->height = 1;
+
+
+    // Translate the cloud into a PointCloud2 message
+    pcl::PCLPointCloud2 cl;
+    sensor_msgs::PointCloud2 output;
+    pcl::toPCLPointCloud2(*basic_cloud_ptr,cl);
+    pcl_conversions::fromPCL(cl, output);
+
+
+    // Publish the message
+    pub.publish(output);
+
+
+
+    // Debug
+    ROS_INFO("Received");
+    // ROS_INFO("Val: %d  Col: %d  Row: %d", intensity.val[0], cv_ptr->image.cols, cv_ptr->image.rows);
 
     // Translate and publish the cv_image
     // pub.publish(cv_ptr->toImageMsg());
+
+
+    // Initialize the viewer
+    viewer->setBackgroundColor (0, 0, 0);
+    viewer->addPointCloud<pcl::PointXYZ> (basic_cloud_ptr, "main cloud");
+    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "main cloud");
+    viewer->addCoordinateSystem (1.0);
+    viewer->initCameraParameters ();
+
+    viewer->spin();  // update the viewer
 
 }
 
