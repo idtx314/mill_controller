0c1d62cbafd053280f5f7f6a79381ea213273370
Ian Davis <idtx314business@gmail.com>
2018-11-11 18:23:24 -0600

    Changed the scaling factor in image_translator.cpp back to 1. Code is still working.

--- src/image_translator.cpp
+++ src/image_translator.cpp
@@ -22,8 +22,8 @@ cv_bridge::CvImagePtr cv_ptr;
 // Create a visualizer
 boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
 
-float scale = .01;
-// float scale = 1;
+// float scale = .01;
+float scale = 1;
 
 
 void callback(const sensor_msgs::Image &msg)
