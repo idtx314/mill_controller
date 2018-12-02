813fc27243c10ef9b6d0ecdab10932111938b9b1
Ian Davis <idtx314business@gmail.com>
2018-12-01 21:42:09 -0600

    They even have the C++11 option added to CMakeLists.txt ahead of time as a comment. Judging fromthe note there and the specific phrasing of the C++ notes at http://www.ros.org/reps/rep-0003.html#c, it sounds like C++11 was soft supported in ROS Kinetic and Lunar before C++14 was made the official standard in Melodic.

--- src/octomap_to_array.cpp
+++ src/octomap_to_array.cpp
@@ -51,9 +51,8 @@ void callback(const octomap_msgs::Octomap &msg)
     y = y/res;
     z = z/res;
     // I need to convert x,y,z into integers to provide the size of the array. In hypothesis they will always be integers, but due to precision limits they will sometimes be very slightly lower than than the appropriate integer value. This causes them to be truncated to a lower value when converted.
-    // So far I have been adding a small amount before casting, but this feels imprecise.
-    // I want to come away with a number that is equal to or larger than the dimensions of the octomap
-    // Can I assume that octomap sizes/res will always be an int?
+    // Can I assume that octomap sizes/res will always be an integer value?
+    // Octomap measurements are measurements of all space in meters. Since resolution will dictate the smallest unit increments of the space (I think) I should be able to rely on the result always being an integer value.
 
 
     // Allocate memory for a 3 dimension char array with dimensions based on octomap size. Coordinate axes follow standard depth image protocol. From reference viewpoint: x to right, y down, z into image.
