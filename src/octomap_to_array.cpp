8d1cc8f3d572614d538f647309d9c14e1f937f0c
Ian Davis <idtx314business@gmail.com>
2018-12-03 20:32:19 -0600

    Removed old array initialization and value setting. Also removed some unused variables leftover from when the node was built.

--- src/octomap_to_array.cpp
+++ src/octomap_to_array.cpp
@@ -41,81 +41,44 @@ void callback(const octomap_msgs::Octomap &msg)
     octree->expand();
 
     // get tree information
-    size_t method_count = octree->getNumLeafNodes();
     double res = octree->getResolution();
-    unsigned int max_depth = octree->getTreeDepth();
 
     // Determine x,y,z dimensions and translate into needed array size
     double x, y, z;
     double xmin, ymin, zmin;
     long xmax, ymax, zmax;
-    char s[100];
     octree->getMetricSize(x, y, z);
     octree->getMetricMin(xmin, ymin, zmin);
+    // Convert from measurement size to node numbers.
     x = x/res;
     y = y/res;
     z = z/res;
 
+    // Round and convert to integers.
     xmax = std::lround(x);
     ymax = std::lround(y);
     zmax = std::lround(z);
 
 
-    // Allocate memory for a 3 dimension char array with dimensions based on octomap size. Coordinate axes follow standard depth image protocol. From reference viewpoint: x to right, y down, z into image.
-        // size of array dimension is maximum index + 1 || MetricSize/res
-        // Add very small amount to eliminate float precision issue. Floats were being truncated to a number below their proper value.
-    char arr[xmax][ymax][zmax];
+    // Create a 3 layer vector array to hold occupancy data.
+    std::vector<std::vector<std::vector<char>>> arr;
 
-    // Create vector of vectors to hold occupancy data.
-    std::vector<std::vector<std::vector<char>>> arr2;
-
-    // Set dimensions of array.
-    arr2.resize(xmax);
+    // Set dimensions of array. Coordinate axes follow standard depth image protocol. From reference viewpoint: x to right, y down, z into image.
+    // size of array dimension is maximum index + 1 || MetricSize/res
+    arr.resize(xmax);
     for(int i=0; i < xmax; i++)
     {
-        arr2[i].resize(ymax);
+        arr[i].resize(ymax);
         for(int j=0; j < ymax; j++)
         {
-            arr2[i][j].resize(zmax);
+            arr[i][j].resize(zmax);
             // Set contents of each vector to 1
-            arr2[i][j].assign(zmax,1);
+            arr[i][j].assign(zmax,1);
         }
     }
 
-
-    // Loop through array and set all values to 0. Both unknown and empty space will be 0 this way.
-    memset(arr, '\0', xmax*ymax*zmax);
-
-    // Create index values
-    long xind=0, yind=0, zind=0;
     // Create Occupancy message
     mill_controller::Occupancy oc_msg;
-
-    // for each leaf of octree (based on leaf iterators)
-    for(octomap::OcTree::leaf_iterator it = octree->begin_leafs(),
-        end = octree->end_leafs();
-        it != end;
-        ++it)
-    {
-        // convert the node center coords to integer indexes for the array.
-            // index = (value-MetricMin)/res-0.5
-        xind = std::lround((it.getX()-xmin)/res - 0.5);
-        yind = std::lround((it.getY()-ymin)/res - 0.5);
-        zind = std::lround((it.getZ()-zmin)/res - 0.5);
-
-        // if node is occupied
-        if(octree->isNodeOccupied(*it))
-        {
-            // set the char at those index values to 1
-            arr[xind][yind][zind] = 1;
-        }
-        else
-        {
-            // set the char at those index values to 0
-            // TODO This is not necessary if all values are initialized to 0.
-            arr[xind][yind][zind] = 0;
-        }
-    }
 
     // Declare a point object for recording Marker message information
     geometry_msgs::Point p;
@@ -125,7 +88,7 @@ void callback(const octomap_msgs::Octomap &msg)
 
     double xcoord, ycoord, zcoord;
 
-    // For each index coordinate in the vector array, set the coordinate based on octomap data.
+    // Cycle through indexes in the vector array and set their value based on octomap data.
     // For each depth
     for(int k=0; k < zmax; k++)
     {
@@ -151,12 +114,12 @@ void callback(const octomap_msgs::Octomap &msg)
                     if(!octree->isNodeOccupied(result))
                     {
                         // Change element to 0
-                        arr2[i][j][k] = 0;
+                        arr[i][j][k] = 0;
 
-                        // Add the point to occupancy message
-                        oc_msg.column.push_back(xind);
-                        oc_msg.row.push_back(yind);
-                        oc_msg.layer.push_back(zind);
+                        // Add the indexes to occupancy message
+                        oc_msg.column.push_back(i);
+                        oc_msg.row.push_back(j);
+                        oc_msg.layer.push_back(k);
                     }
                     else // Node is occupied
                     {
@@ -169,12 +132,12 @@ void callback(const octomap_msgs::Octomap &msg)
                 }
                 else // Assume node is unoccupied
                 {
-                    arr2[i][j][k] = 0;
+                    arr[i][j][k] = 0;
 
-                    // Add the point to occupancy message
-                    oc_msg.column.push_back(xind);
-                    oc_msg.row.push_back(yind);
-                    oc_msg.layer.push_back(zind);
+                    // Add the indexes to occupancy message
+                    oc_msg.column.push_back(i);
+                    oc_msg.row.push_back(j);
+                    oc_msg.layer.push_back(k);
                 }
             }
         }
@@ -187,7 +150,7 @@ void callback(const octomap_msgs::Octomap &msg)
         {
             for(int i=0; i < xmax; i++)
             {
-                printf("%d",arr2[i][j][k]);
+                printf("%d",arr[i][j][k]);
             }
             printf("\n");
         }
