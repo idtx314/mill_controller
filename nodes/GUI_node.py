5d3d3650b630bf1eb402bee7bc1711f6b53967e0
Ian Davis <idtx314business@gmail.com>
2018-12-06 15:06:35 -0600

    Practicing some basic cv2 functions and getting reference image set up.

--- nodes/GUI_node.py
+++ nodes/GUI_node.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python
 
+import cv2
+import rospkg
 
 
 
+def main():
 
+    # Open image, using file images for now
+    rospack = rospkg.RosPack()
+    path = rospack.get_path('mill_controller') + '/images/shapes.jpg'
+    img = cv2.imread(path,cv2.IMREAD_COLOR)
 
+    # Show image
+    cv2.imshow('window',img)
 
+    cv2.waitKey(0)
 
+    img = cv2.resize(img, (640,480))
+
+    cv2.imshow('window',img)
+    cv2.waitKey(0)
+
+    cv2.imwrite('resize.jpg',img)
+
+    cv2.destroyAllWindows()
 
 
 
