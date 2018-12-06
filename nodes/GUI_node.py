efdbec25fb0fd0f51c8c43d5287023d56e164765
Ian Davis <idtx314business@gmail.com>
2018-12-06 15:46:48 -0600

    Add some text too.

--- nodes/GUI_node.py
+++ nodes/GUI_node.py
@@ -9,54 +9,64 @@ def main():
 
     # Open image, using file images for now
     rospack = rospkg.RosPack()
-    path = rospack.get_path('mill_controller') + '/images/shapes.jpg'
+    path = rospack.get_path('mill_controller') + '/images/resize.jpg'
     img = cv2.imread(path,cv2.IMREAD_COLOR)
 
     # Show image
     cv2.imshow('window',img)
     cv2.waitKey(0)
 
-    # # Draw
-    # img_copy = img.copy()
-    # center = (200,150)
-    # radius = 10
-    # color = (0,0,255)
-    # cv2.circle(img_copy,center, radius,color)
+    # Draw
+    img_copy = img.copy()
+    center = (200,150)
+    radius = 10
+    color = (0,0,255)
+    cv2.circle(img_copy,center, radius,color)
 
-    # start = (5, 5)
-    # end = (100, 200)
-    # cv2.line(img_copy,start,end,color)
+    start = (5, 5)
+    end = (100, 200)
+    cv2.line(img_copy,start,end,color)
 
-    # # Show image
-    # cv2.imshow('window',img_copy)
-    # cv2.waitKey(0)
-
-
-
-
+    str = 'Input text'
+    text_origin = (400,400)
+    fontFace = cv2.FONT_HERSHEY_PLAIN
+    fontScale = 1
+    cv2.putText(img_copy, str, text_origin,fontFace,fontScale,color)
 
 
 
 
 
-
-
-
-
-
-    # Image resizing code. My reference was much too large
-    img = cv2.resize(img, (640,int(640/1.5)))  # Resize to given (width,height)
-    # img = cv2.resize(img, (0,0), fx=.3, fy=.3)  # Halve image size
-
-    cv2.imshow('window',img)
+    # Show image
+    cv2.imshow('window',img_copy)
     cv2.waitKey(0)
 
-    path = rospack.get_path('mill_controller') + '/images/resize.jpg'
-    cv2.imwrite(path,img)
 
 
-    # Close image windows for shutdown
-    cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+    # # Image resizing code. My reference was much too large
+    # img = cv2.resize(img, (640,int(640/1.5))  # Resize to given (width,height)
+    # # img = cv2.resize(img, (0,0), fx=.3, fy=.3)  # Halve image size
+
+    # cv2.imshow('window',img)
+    # cv2.waitKey(0)
+
+    # path = rospack.get_path('mill_controller') + '/images/resize.jpg'
+    # cv2.imwrite(path,img)
+
+
+    # # Close image windows for shutdown
+    # cv2.destroyAllWindows()
 
 
 
